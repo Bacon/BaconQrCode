4ecc614c1abf4e409c9225f94fe9d24d9f294175
Artur Bodera <abodera@gmail.com>
2013-04-13 10:50:37 +0200

    Add working "Text" renderer backend.

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -11,6 +11,7 @@ return array(
     'BaconQrCode\Renderer\Backend\BackendInterface'     => __DIR__ . '/src/BaconQrCode/Renderer/Backend/BackendInterface.php',
     'BaconQrCode\Renderer\Backend\Svg'                  => __DIR__ . '/src/BaconQrCode/Renderer/Backend/Svg.php',
     'BaconQrCode\Renderer\Backend\Bitmap'               => __DIR__ . '/src/BaconQrCode/Renderer/Backend/Bitmap.php',
+    'BaconQrCode\Renderer\Backend\Text'                 => __DIR__ . '/src/BaconQrCode/Renderer/Backend/Text.php',
     'BaconQrCode\Renderer\RendererInterface'            => __DIR__ . '/src/BaconQrCode/Renderer/RendererInterface.php',
     'BaconQrCode\Renderer\Decorator\FinderPattern'      => __DIR__ . '/src/BaconQrCode/Renderer/Decorator/FinderPattern.php',
     'BaconQrCode\Renderer\Decorator\DecoratorInterface' => __DIR__ . '/src/BaconQrCode/Renderer/Decorator/DecoratorInterface.php',
