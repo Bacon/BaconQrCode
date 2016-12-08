4238048f1ffd8990207ffe37b45749587753acea
Chopin Ngo <consatan@gmail.com>
2016-12-08 21:38:45 +0800

    Add `compact` option on Text renderer

--- src/BaconQrCode/Renderer/Text/Plain.php
+++ src/BaconQrCode/Renderer/Text/Plain.php
@@ -26,6 +26,13 @@ class Plain implements RendererInterface
     protected $margin = 1;
 
     /**
+     * Whether QR code render compact.
+     *
+     * @var boolean
+     */
+    protected $compact = false;
+
+    /**
      * Char used for full block.
      *
      * UTF-8 FULL BLOCK (U+2588)
@@ -34,6 +41,24 @@ class Plain implements RendererInterface
      * @link http://www.fileformat.info/info/unicode/char/2588/index.htm
      */
     protected $fullBlock = "\xE2\x96\x88";
+
+    /**
+     * Char used for upper half block.
+     *
+     * UTF-8 UPPER HALF BLOCK (U+2580)
+     * @var string
+     * @link http://www.fileformat.info/info/unicode/char/2580/index.htm
+     */
+    protected $upperHalfBlock = "\xE2\x96\x80";
+
+    /**
+     * Char used for lower half block.
+     *
+     * UTF-8 LOWER HALF BLOCK (U+2584)
+     * @var string
+     * @link http://www.fileformat.info/info/unicode/char/2584/index.htm
+     */
+    protected $lowerHalfBlock = "\xE2\x96\x84";
 
     /**
      * Char used for empty space
@@ -60,6 +85,46 @@ class Plain implements RendererInterface
     public function getFullBlock()
     {
         return $this->fullBlock;
+    }
+
+    /**
+     * Set char used as upper half block (upper half occupied space, lower half empty space).
+     *
+     * @param string $upperHalfBlock
+     */
+    public function setUpperHalfBlock($upperHalfBlock)
+    {
+        $this->upperHalfBlock = $upperHalfBlock;
+    }
+
+    /**
+     * Get char used as upper half block (upper half occupied space, lower half empty space).
+     *
+     * @return string
+     */
+    public function getUpperHalfBlock()
+    {
+        return $this->upperHalfBlock;
+    }
+
+    /**
+     * Set char used as lower half block (upper half empty space, lower half occupied space).
+     *
+     * @param string $lowerHalfBlock
+     */
+    public function setLowerHalfBlock($lowerHalfBlock)
+    {
+        $this->lowerHalfBlock = $lowerHalfBlock;
+    }
+
+    /**
+     * Get char used as lower half block (upper half empty space, lower half occupied space).
+     *
+     * @return string
+     */
+    public function getLowerHalfBlock()
+    {
+        return $this->lowerHalfBlock;
     }
 
     /**
@@ -111,6 +176,26 @@ class Plain implements RendererInterface
     }
 
     /**
+     * Sets whether QR code render compact.
+     *
+     * @param boolean $compact
+     */
+    public function setCompact($compact)
+    {
+        $this->compact = (bool)$compact;
+    }
+
+    /**
+     * Gets whether QR code render compact.
+     *
+     * @return boolean
+     */
+    public function getCompact()
+    {
+        return $this->compact;
+    }
+
+    /**
      * render(): defined by RendererInterface.
      *
      * @see    RendererInterface::render()
@@ -122,6 +207,7 @@ class Plain implements RendererInterface
         $result = '';
         $matrix = $qrCode->getMatrix();
         $width  = $matrix->getWidth();
+        $margin = str_repeat($this->emptyBlock, $this->margin);
 
         // Top margin
         for ($x = 0; $x < $this->margin; $x++) {
@@ -131,13 +217,47 @@ class Plain implements RendererInterface
         // Body
         $array = $matrix->getArray();
 
-        foreach ($array as $row) {
-            $result .= str_repeat($this->emptyBlock, $this->margin); // left margin
-            foreach ($row as $byte) {
-                $result .= $byte ? $this->fullBlock : $this->emptyBlock;
+        if ($this->compact) {
+            $len = $array->getSize();
+            $size = $array[0]->getSize();
+
+            // If not an even rows, fill an empty blocks row
+            if ($len % 2 !== 0) {
+                $array->setSize($len + 1);
+                for ($i = 0; $i < $size; $i++) {
+                    $array[$len][$i] = 0;
+                }
+                $len++;
             }
-            $result .= str_repeat($this->emptyBlock, $this->margin); // right margin
-            $result .= "\n";
+
+            for ($i = 0; $i < $len; $i++) {
+                // Each loop get 2 rows
+                $even = $array[$i];
+                $odd = $array[++$i];
+
+                $result .= $margin; // left margin
+                for ($j = 0; $j < $size; $j++) {
+                    $upper = $even[$j];
+                    $lower = $odd[$j];
+
+                    if ($upper) {
+                        $result .= $lower ? $this->fullBlock : $this->upperHalfBlock;
+                    } else {
+                        $result .= $lower ? $this->lowerHalfBlock : $this->emptyBlock;
+                    }
+                }
+                $result .= $margin; // right margin
+                $result .= "\n";
+            }
+        } else {
+            foreach ($array as $row) {
+                $result .= $margin; // left margin
+                foreach ($row as $byte) {
+                    $result .= $byte ? $this->fullBlock : $this->emptyBlock;
+                }
+                $result .= $margin; // right margin
+                $result .= "\n";
+            }
         }
 
         // Bottom margin
--- tests/BaconQrCode/Renderer/Text/TextTest.php
+++ tests/BaconQrCode/Renderer/Text/TextTest.php
@@ -69,6 +69,35 @@ class PlainTest extends TestCase
         $this->assertEquals($expected, $this->renderer->render($qrCode));
     }
 
+    public function testBaseCompactRender()
+    {
+        $content = 'foobar';
+        $expected =
+            "                       \n" .
+            " █▀▀▀▀▀█ ▀█▀█▀ █▀▀▀▀▀█ \n" .
+            " █ ███ █  ██▄  █ ███ █ \n" .
+            " █ ▀▀▀ █   ▀▄█ █ ▀▀▀ █ \n" .
+            " ▀▀▀▀▀▀▀ █▄█▄█ ▀▀▀▀▀▀▀ \n" .
+            " ▀▀ ██ ▀  ██ █ █ ▄▄  ▀ \n" .
+            "  ▀▀▀▀▀▀▀▀ █▄ ▀▀ ▀ ▄▀█ \n" .
+            "  ▀▀  ▀▀▀▄ █▄▄ ▀  █ ▄▀ \n" .
+            " █▀▀▀▀▀█  ▀█▄██▀▀▀█▄   \n" .
+            " █ ███ █ ██ ██ ▀█ █▄▀▀ \n" .
+            " █ ▀▀▀ █ ▄▄█  ▄█▄▀█▄██ \n" .
+            " ▀▀▀▀▀▀▀ ▀▀▀▀   ▀▀     \n" .
+            "                       \n"
+        ;
+
+        $qrCode = Encoder::encode(
+            $content,
+            new ErrorCorrectionLevel(ErrorCorrectionLevel::L),
+            Encoder::DEFAULT_BYTE_MODE_ECODING
+        );
+        $this->renderer->setCompact(true);
+        $this->assertEquals(true, $this->renderer->getCompact());
+        $this->assertEquals($expected, $this->renderer->render($qrCode));
+    }
+
     public function testBasicRenderNoMargins()
     {
         $content = 'foobar';
@@ -103,6 +132,35 @@ class PlainTest extends TestCase
         );
         $this->renderer->setMargin(0);
         $this->assertEquals(0, $this->renderer->getMargin());
+        $this->assertEquals($expected, $this->renderer->render($qrCode));
+    }
+
+    public function testBaseCompactRenderNoMargins()
+    {
+        $content = 'foobar';
+        $expected =
+            "█▀▀▀▀▀█ ▀█▀█▀ █▀▀▀▀▀█\n" .
+            "█ ███ █  ██▄  █ ███ █\n" .
+            "█ ▀▀▀ █   ▀▄█ █ ▀▀▀ █\n" .
+            "▀▀▀▀▀▀▀ █▄█▄█ ▀▀▀▀▀▀▀\n" .
+            "▀▀ ██ ▀  ██ █ █ ▄▄  ▀\n" .
+            " ▀▀▀▀▀▀▀▀ █▄ ▀▀ ▀ ▄▀█\n" .
+            " ▀▀  ▀▀▀▄ █▄▄ ▀  █ ▄▀\n" .
+            "█▀▀▀▀▀█  ▀█▄██▀▀▀█▄  \n" .
+            "█ ███ █ ██ ██ ▀█ █▄▀▀\n" .
+            "█ ▀▀▀ █ ▄▄█  ▄█▄▀█▄██\n" .
+            "▀▀▀▀▀▀▀ ▀▀▀▀   ▀▀    \n"
+        ;
+
+        $qrCode = Encoder::encode(
+            $content,
+            new ErrorCorrectionLevel(ErrorCorrectionLevel::L),
+            Encoder::DEFAULT_BYTE_MODE_ECODING
+        );
+        $this->renderer->setMargin(0);
+        $this->renderer->setCompact(true);
+        $this->assertEquals(0, $this->renderer->getMargin());
+        $this->assertEquals(true, $this->renderer->getCompact());
         $this->assertEquals($expected, $this->renderer->render($qrCode));
     }
 
@@ -144,6 +202,43 @@ class PlainTest extends TestCase
         $this->renderer->setEmptyBlock('-');
         $this->assertEquals('#', $this->renderer->getFullBlock());
         $this->assertEquals('-', $this->renderer->getEmptyBlock());
+        $this->assertEquals($expected, $this->renderer->render($qrCode));
+    }
+
+    public function testBaseCompactRenderCustomChar()
+    {
+        $content = 'foobar';
+        $expected =
+            ".......................\n" .
+            ".#¯¯¯¯¯#.¯#¯#¯.#¯¯¯¯¯#.\n" .
+            ".#.###.#..##_..#.###.#.\n" .
+            ".#.¯¯¯.#...¯_#.#.¯¯¯.#.\n" .
+            ".¯¯¯¯¯¯¯.#_#_#.¯¯¯¯¯¯¯.\n" .
+            ".¯¯.##.¯..##.#.#.__..¯.\n" .
+            "..¯¯¯¯¯¯¯¯.#_.¯¯.¯._¯#.\n" .
+            "..¯¯..¯¯¯_.#__.¯..#._¯.\n" .
+            ".#¯¯¯¯¯#..¯#_##¯¯¯#_...\n" .
+            ".#.###.#.##.##.¯#.#_¯¯.\n" .
+            ".#.¯¯¯.#.__#.._#_¯#_##.\n" .
+            ".¯¯¯¯¯¯¯.¯¯¯¯...¯¯.....\n" .
+            ".......................\n"
+        ;
+
+        $qrCode = Encoder::encode(
+            $content,
+            new ErrorCorrectionLevel(ErrorCorrectionLevel::L),
+            Encoder::DEFAULT_BYTE_MODE_ECODING
+        );
+        $this->renderer->setCompact(true);
+        $this->renderer->setFullBlock('#');
+        $this->renderer->setEmptyBlock('.');
+        $this->renderer->setUpperHalfBlock('¯');
+        $this->renderer->setLowerHalfBlock('_');
+        $this->assertEquals(true, $this->renderer->getCompact());
+        $this->assertEquals('#', $this->renderer->getFullBlock());
+        $this->assertEquals('.', $this->renderer->getEmptyBlock());
+        $this->assertEquals('¯', $this->renderer->getUpperHalfBlock());
+        $this->assertEquals('_', $this->renderer->getLowerHalfBlock());
         $this->assertEquals($expected, $this->renderer->render($qrCode));
     }
 }
