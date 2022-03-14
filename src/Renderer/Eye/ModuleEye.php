da293f8ce5dd51f8eef9489fc70aa93896c000c1
MustangGB <mustang@miswave.com>
2022-03-14 00:51:07 +0000

    Update ModuleEye.php

--- src/Renderer/Eye/ModuleEye.php
+++ src/Renderer/Eye/ModuleEye.php
@@ -22,7 +22,7 @@ final class ModuleEye implements EyeInterface
         $this->module = $module;
     }
 
-    public function getExternalPath() : Path
+    public function getExternalPath($location) : Path
     {
         $matrix = new ByteMatrix(7, 7);
 
@@ -39,7 +39,7 @@ final class ModuleEye implements EyeInterface
         return $this->module->createPath($matrix)->translate(-3.5, -3.5);
     }
 
-    public function getInternalPath() : Path
+    public function getInternalPath($location) : Path
     {
         $matrix = new ByteMatrix(3, 3);
 
